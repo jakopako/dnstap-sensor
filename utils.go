6a47e0a87bcdccd3480fc8aadbb37f6400a5eb9a
jakopako <jakob.dhondt@web.de>
2021-01-23 12:13:56 +0100

    dnsquerybuffer + tests

--- utils.go
+++ utils.go
@@ -1,29 +1,61 @@
 package main
 
-import "container/list"
+import (
+	"crypto/sha256"
+	"strconv"
+)
 
 // DNSQueryBuffer buffers DNS queries.
 type DNSQueryBuffer struct {
-	buckets    *list.List
-	windowSize int //in seconds
+	buckets              []map[[sha256.Size]byte]bool // The buckets contain a set of hashes. (https://yourbasic.org/golang/implement-set/)
+	currentBucketPointer int                          // The bucket where the hashes are currently written to.
 }
 
 func newDNSQueryBuffer(windowSize int) *DNSQueryBuffer {
+	b := make([]map[[sha256.Size]byte]bool, windowSize)
+	for i := range b {
+		b[i] = make(map[[sha256.Size]byte]bool)
+	}
 	return &DNSQueryBuffer{
-		buckets:    list.New(),
-		windowSize: windowSize,
+		buckets:              b,
+		currentBucketPointer: 0,
 	}
 }
 
+func queryToHash(queryID uint16, queryPort uint32, qname string, qtype uint16) [sha256.Size]byte {
+	s := strconv.FormatUint(uint64(queryID), 10) +
+		strconv.FormatUint(uint64(queryPort), 10) +
+		qname +
+		strconv.FormatUint(uint64(qtype), 10)
+	return sha256.Sum256([]byte(s))
+}
+
 // addQuery adds a new DNS query to the DNSQueryBuffer.
-func (db *DNSQueryBuffer) addQuery(queryID uint16, queryPort uint32, qname string, qtype uint16) {}
+func (db *DNSQueryBuffer) addQuery(queryID uint16, queryPort uint32, qname string, qtype uint16) {
+	db.buckets[db.currentBucketPointer][queryToHash(queryID, queryPort, qname, qtype)] = true
+}
 
 // run starts the sliding window mechanism where old entries
 // that have not been used (i.e found through IsInBuffer) are deleted.
 func (db *DNSQueryBuffer) run() {}
 
+// moveCurrentBucketPointer moves the currentBucketPointer by one in the negative direction
+// and clears the new current bucket's map.
+func (db *DNSQueryBuffer) moveCurrentBucketPointer() {
+	db.currentBucketPointer = (db.currentBucketPointer - 1 + len(db.buckets)) % len(db.buckets)
+	db.buckets[db.currentBucketPointer] = make(map[[sha256.Size]byte]bool)
+}
+
 // isInBuffer checks whether the specified combination of a, b, c
 // is in the buffer and if so deletes this specific entry.
 func (db *DNSQueryBuffer) isInBuffer(queryID uint16, queryPort uint32, qname string, qtype uint16) bool {
+	h := queryToHash(queryID, queryPort, qname, qtype)
+	for i := db.currentBucketPointer; i < db.currentBucketPointer+len(db.buckets); i++ {
+		j := i % len(db.buckets)
+		if _, ok := db.buckets[j][h]; ok {
+			delete(db.buckets[j], h)
+			return true
+		}
+	}
 	return false
 }
---
+++ utils_test.go
@@ -0,0 +1,84 @@
+package main
+
+import "testing"
+
+const (
+	queryID    uint16 = 1
+	queryPort  uint32 = 5000
+	qname      string = "example.com"
+	qtype      uint16 = 1
+	queryID1   uint16 = 2
+	queryPort1 uint32 = 3000
+	qname1     string = "google.com"
+	qtype1     uint16 = 28
+)
+
+// TestIsInBuffer0 tests whether a query can be added to the buffer
+// and found by IsInBuffer. This test is entirely static, i.e.
+// the mechanism that removes old queries is not running.
+func TestIsInBuffer0(t *testing.T) {
+	db := newDNSQueryBuffer(10)
+	db.addQuery(queryID, queryPort, qname, qtype)
+	if !db.isInBuffer(queryID, queryPort, qname, qtype) {
+		t.Errorf("Query is not in buffer but should be.")
+	}
+}
+
+// TestIsInBuffer1 is very similar to TestIsInBuffer0 but checks
+// for the same DNS query a second time to verify that after checking
+// once it's not in the buffer anymore.
+func TestIsInBuffer1(t *testing.T) {
+	db := newDNSQueryBuffer(10)
+	db.addQuery(queryID, queryPort, qname, qtype)
+	if !db.isInBuffer(queryID, queryPort, qname, qtype) {
+		t.Errorf("Query is not in buffer but should be.")
+	}
+	if db.isInBuffer(queryID, queryPort, qname, qtype) {
+		t.Errorf("Query is still in buffer but should not be anymore.")
+	}
+}
+
+func TestIsInBuffer2(t *testing.T) {
+	db := newDNSQueryBuffer(10)
+	db.addQuery(queryID, queryPort, qname, qtype)
+	db.addQuery(queryID1, queryPort1, qname1, qtype1)
+	if !db.isInBuffer(queryID, queryPort, qname, qtype) {
+		t.Errorf("Query with qname %s is not in buffer but should be.", qname)
+	}
+	if !db.isInBuffer(queryID1, queryPort1, qname1, qtype1) {
+		t.Errorf("Query with qname %s is not in buffer but should be.", qname)
+	}
+	if db.isInBuffer(queryID, queryPort, qname, qtype) {
+		t.Errorf("Query with qname %s is still in buffer but should not be anymore.", qname)
+	}
+	if db.isInBuffer(queryID1, queryPort1, qname1, qtype1) {
+		t.Errorf("Query with qname %s is still in buffer but should not be anymore.", qname)
+	}
+}
+
+func TestMoveCurrentBucketPointer1(t *testing.T) {
+	db := newDNSQueryBuffer(5)
+	for i := 1; i < 5; i++ {
+		db.addQuery(queryID, queryPort, qname, qtype)
+		for j := 1; j <= i; j++ {
+			db.moveCurrentBucketPointer()
+		}
+		if !db.isInBuffer(queryID, queryPort, qname, qtype) {
+			t.Errorf("Query is not in buffer but should be.")
+		}
+		if db.isInBuffer(queryID, queryPort, qname, qtype) {
+			t.Errorf("Query is still in buffer but should not be anymore.")
+		}
+	}
+}
+
+func TestMoveCurrentBucketPointer2(t *testing.T) {
+	db := newDNSQueryBuffer(5)
+	db.addQuery(queryID, queryPort, qname, qtype)
+	for j := 1; j <= 5; j++ {
+		db.moveCurrentBucketPointer()
+	}
+	if db.isInBuffer(queryID, queryPort, qname, qtype) {
+		t.Errorf("Query is still in buffer but should not be anymore.")
+	}
+}
